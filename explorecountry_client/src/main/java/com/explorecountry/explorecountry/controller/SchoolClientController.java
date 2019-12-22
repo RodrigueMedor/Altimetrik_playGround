3512230a6f5e9340f062f8d855fe3e394e89afef
Medor Rodrigue <rodriguemedor@yahoo.fr>
2019-12-22 16:15:57 -0600

    css update one

--- explorecountry_client/src/main/java/com/explorecountry/explorecountry/controller/SchoolClientController.java
+++ explorecountry_client/src/main/java/com/explorecountry/explorecountry/controller/SchoolClientController.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class WebController {
+public class SchoolClientController {
 	
 	
 	@RequestMapping(value="/")
 	public String input() {
 		
-		return "index";
+		return "test";
 	}
 
 }
